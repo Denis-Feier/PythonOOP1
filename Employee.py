e95834d0be8eee1d7876c26818e8d373b89dc2fb
Denis Feier <denisfeier98@yahoo.com>
2019-02-27 22:25:32 +0200

    let the fun begin

--- Employee.py
+++ Employee.py
@@ -1,2 +1,21 @@
 class Employee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@gmail.com'
+
+    def fullname(self):
+        return '{} {}\n'.format(self.first, self.last, self.email)
+
+
+def main():
+    emp1 = Employee('Denis', 'Feier', 3000)
+    emp2 = Employee('Sergiu', 'Radu', 4000)
+
+    print(emp1.fullname())
+    print(emp2.fullname())
+
+
+if __name__ == '__main__':
+    main()
