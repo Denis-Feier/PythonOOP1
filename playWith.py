ba5ce35e68afedfbd9856c7033fe4ac66f4689dc
Denis Feier <denisfeier98@yahoo.com>
2019-03-02 19:03:49 +0200

    let the fun begin

---
+++ playWith.py
@@ -0,0 +1,22 @@
+from src.Employee import Employee
+from src.Manager import Manager
+
+
+def argumente(*arg):
+    return arg
+
+
+def main():
+
+    emp1 = Employee('Denisu', 'Doarme 3 ore', 2000)
+    emp2 = Employee('Mircea', 'Random', 1000000)
+    man1 = Manager('oFemeie', "random", 10000)
+
+    print(emp1)
+    print(repr(emp2))
+    print(repr(emp2 + emp1))
+    print(argumente(man1,emp1, emp2))
+
+
+if __name__ == '__main__':
+    main()
